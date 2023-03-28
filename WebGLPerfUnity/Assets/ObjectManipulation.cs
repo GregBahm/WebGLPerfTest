82dd20b0045c80280f1211279705672647a3f543
Greg Bahm <grbahm@microsoft.com>
2023-03-27 23:11:01 -0700

    A test.

---
+++ WebGLPerfUnity/Assets/ObjectManipulation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectManipulation : MonoBehaviour
+{
+    public Transform CameraOrbitTransform;
+    public Camera Camera;
+
+    public float XRotationSpeed;
+    public float YRotationSpeed;
+
+    [Range(0, 1)]
+    public float Zoom;
+    public float MaxCameraDistance;
+    public float MinCameraDistance;
+    private Vector3 cameraStartPosition;
+    private Quaternion cameraStartRotation;
+    public float MousewheelZoomSpeed;
+
+    private void Start()
+    {
+        cameraStartPosition = CameraOrbitTransform.position;
+        cameraStartRotation = CameraOrbitTransform.rotation;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoReset();
+        }
+
+        bool uiHovered = UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
+        if (!uiHovered)
+        {
+            UpdateZoom();
+            HandleCameraOrbit();
+        }
+        UpdateCameraForZoom();
+    }
+
+    private void UpdateZoom()
+    {
+        float delta = Input.mouseScrollDelta.y* MousewheelZoomSpeed;
+        Zoom = Mathf.Clamp01(Zoom + delta);
+    }
+
+    private void UpdateCameraForZoom()
+    {
+        float cameraZ = Mathf.Lerp(MinCameraDistance, MaxCameraDistance, Zoom);
+        Camera.transform.localPosition = new Vector3(0, 0, cameraZ);
+    }
+
+    private void DoReset()
+    {
+        CameraOrbitTransform.rotation = cameraStartRotation;
+        Zoom = 1f;
+    }
+
+    private Vector2 startOrbitMouse;
+    private Vector3 startOrbitCamera;
+
+    private void HandleCameraOrbit()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startOrbitMouse = Input.mousePosition;
+            startOrbitCamera = CameraOrbitTransform.eulerAngles;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            float xDelta = startOrbitMouse.x - Input.mousePosition.x;
+            float xRot = xDelta * XRotationSpeed;
+            float yDelta = startOrbitMouse.y - Input.mousePosition.y;
+            float yRot = yDelta * YRotationSpeed;
+            CameraOrbitTransform.rotation = Quaternion.Euler(startOrbitCamera.x + yRot, startOrbitCamera.y + xRot, 0);
+        }
+    }
+}
--- WebGLPerfUnity/Assets/WebGLPerfController.cs
+++ WebGLPerfUnity/Assets/WebGLPerfController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using TMPro;
 using UnityEngine;
@@ -7,14 +9,71 @@ using UnityEngine;
 public class WebGLPerfController : MonoBehaviour
 {
     public TextMeshProUGUI FpsLabel;
+    public TextMeshProUGUI DrawCallsLabel;
+
+    public GameObject[] CubeSets;
+    [SerializeField]
+    private int batchSizeIndex;
+    [SerializeField]
+    private int totalCountIndex;
+    public GameObject Root;
 
     public const int MaxFrames = 30;
     private float[] frameTimes = new float[MaxFrames];
     private int frameCounterIndex;
 
+
     private void Update()
     {
         UpdateFps();
+    }
+
+    public void SetBatchSize(int index)
+    {
+        batchSizeIndex = index;
+    }
+
+    public void SetTotalCountIndex(int index)
+    {
+        totalCountIndex = index;
+    }
+
+    public void SpawnCubes()
+    {
+        //Out with the old
+        Destroy(Root);
+        Root = new GameObject();
+
+        int sideLength = GetSideSize();
+        GameObject set = CubeSets[batchSizeIndex];
+        Vector3 center = GetLocalPosition(sideLength, sideLength, sideLength) * .5f;
+        for (int x = 0; x < sideLength; x++)
+        {
+            for (int y = 0; y < sideLength; y++)
+            {
+                for (int z = 0; z < sideLength; z++)
+                {
+                    GameObject obj = Instantiate(set);
+                    obj.transform.SetParent(Root.transform, false);
+                    obj.transform.localPosition = GetLocalPosition(x, y, z) - center;
+                    obj.transform.localScale = new Vector3(.9f, .9f, .9f);
+                }
+            }
+        }
+        int objs = (int)Mathf.Pow(sideLength, 3);
+        DrawCallsLabel.text = "Draw Calls: " + objs;
+    }
+
+    private Vector3 GetLocalPosition(int x, int y, int z)
+    {
+        int baseIncrement = (int)Mathf.Pow(2, batchSizeIndex);
+        return new Vector3(-baseIncrement * x, -baseIncrement * y, baseIncrement * z);
+    }
+
+    private int GetSideSize()
+    {
+        int power = (totalCountIndex + 5) - batchSizeIndex;
+        return (int)Mathf.Pow(2, power);
     }
 
     private void UpdateFps()
